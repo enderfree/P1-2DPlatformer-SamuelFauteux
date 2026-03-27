dbe0b6c5d6e11142816e80ab1dc9b542ca14ce89
Samuel Fauteux <samuelfauteux7@gmail.com>
2026-03-27 18:34:19 -0400

    Moved MainMenu.cs
    Added comment and moved main menu script in script folder.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    // No real logic place to talk of my render mode since I'm not touching it in the 
+    // code, but if you go look in the editor, my canvas is Screen Space - Overlay as 
+    // I want it to be at the top of the screen, at the forefront of everything. 
+
     [SerializeField] private GameObject buttonPanel;
     [SerializeField] private GameObject settingPannel;
 
